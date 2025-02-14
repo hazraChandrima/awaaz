9c107f263f379514c1ad628ac09875d061c46f48
Chandrima Hazra <hazrachandrima6@gmail.com>
2025-02-14 10:01:03 +0530

    added basic folder structure

---
+++ src/app/auth/register.tsx
@@ -0,0 +1,11 @@
+const Register = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-center mb-4">mobile navbar</h2>
+    </div>
+  </div>;
+};
+
+export default Footer;
