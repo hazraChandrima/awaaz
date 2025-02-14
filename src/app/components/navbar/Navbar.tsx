c2d998a36ffa90c245fec164661a6a0d17905b19
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-02-14 11:29:59 +0530

    added site logo

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import logo from '../../../../public/assets/logo.jpeg';
+import Image from 'next/image';
 
 function Navbar() {
   return (
     <header className="flex flex-wrap  md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200">
       <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 p-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center gap-x-1">
-          <a className="flex-none font-bold text-xl text-[#CA3C25] focus:outline-none focus:opacity-80" href="#" aria-label="Brand">Awaaz</a>
+          <a className="flex-none font-bold text-xl text-[#CA3C25] focus:outline-none focus:opacity-80" href="#" aria-label="Brand">
+            <Image src={logo} className='h-[35px] w-[100px] md:h-[70px] md:w-[250px]' alt='site logo' />
+          </a>
     
           <button type="button" className="hs-collapse-toggle md:hidden relative size-9 flex justify-center items-center font-medium text-[12px] rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none" id="hs-header-base-collapse"  aria-expanded="false" aria-controls="hs-header-base" aria-label="Toggle navigation"  data-hs-collapse="#hs-header-base" >
             <svg className="hs-collapse-open:hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
