6b5585affb9d21d832f61abeea09bd58d52d4160
Chandrima Hazra <hazrachandrima6@gmail.com>
2025-02-14 11:54:20 +0530

    added clerk for user authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
 import type { Metadata } from "next";
-import {Armata } from "next/font/google";
+import { Armata } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import PrelineScript from "./components/PrelineScript";
-import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Awaaz",
   description: "Raise your voice, Shape the change.",
 };
 
-const armata = Armata({subsets:['latin'], weight: '400', display: 'swap'});
+const armata = Armata({ subsets: ["latin"], weight: "400", display: "swap" });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${armata.className} antialiased`}
-      >
-        <Navbar/>
-        {children}
-      </body>
-      <PrelineScript/>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${armata.className} antialiased`}>
+          <Navbar />
+          <main className="container mx-auto p-4">{children}</main>
+        </body>
+        <PrelineScript />
+      </html>
+    </ClerkProvider>
   );
 }
