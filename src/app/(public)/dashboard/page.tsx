243c973a7843465c5875d3c9fc9b615587c212fb
hemanvithapullela0456 <hemanvithapullela@gmail.com>
2025-02-15 22:59:58 +0530

    made changes to styling

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 const Dashboard = () => {
     return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex mt-20 min-h-screen items-center justify-center bg-gray-100">
 
       </div>
     );
